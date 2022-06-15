918ad45a99160cda890cac9ced521da75e618a15
AnotherTwinkle <anothersparklingstar@gmail.com>
2022-06-15 11:57:14 +0600

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+#define TestCase() int ts; cin >> ts; while(ts--)
+#define printvec(a) for(auto& i : (a)) cout << i << " "; cout << endl
+#define printpair(a) cout << "(" << (a).ff  << " "<< (a).ss << ")" << endl
+#define returncout(s) cout << ((s)) << endl; return
+
+#define all(v) (v).begin(), (v).end()
+#define size(x) (int)(x).size()
+#define ff first
+#define ss second
+
+const long long llzero = 0;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(size(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+	setIO();
+
+}
